9329c8296a2a2eaeec932bd7dbbb30864a2de43f
ducminhnguyen1411hn <169039461+ducminhnguyen1411hn@users.noreply.github.com>
2024-05-11 18:54:30 +0700

    update slot 2

---
+++ PRN231/code/SE1702-PRN231/SE1702-PRN231/Controllers/HomeController.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SE1702_PRN231.Models;
+using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+
+namespace SE1702_PRN231.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly MySaleDBContext dbContext;
+
+        public List<Product> Products { get; set; }
+
+        public Product CurrentProduct { get; set; }
+
+        public int PickCategory { get; set; }
+
+        public List<Category> Categories { get; set; }
+
+        public List<string> ColumnName { get; set; }
+
+        public HomeController(ILogger<HomeController> logger, MySaleDBContext mySaleDbContext)
+        {
+            _logger = logger;
+            dbContext = mySaleDbContext;
+            ColumnName = new List<string>();
+        }
+
+
+        
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            if (!(product.ProductName.Length == 0 || 
+                product.Image.Length == 0 || product.UnitPrice is not decimal || product.UnitsInStock is not int || product.CategoryId == 0))
+                
+            {
+                dbContext.Products.Add(product);
+                dbContext.SaveChanges();
+            }
+            
+            
+            return RedirectToAction("List");
+        }
+
+        [HttpPost]
+        public IActionResult UpdateProduct(Product product)
+        {
+            if (!(product.ProductName.Length == 0 ||
+                product.Image.Length == 0 || product.UnitPrice is not decimal || product.UnitsInStock is not int || product.CategoryId == 0))
+
+            {
+                var updateProduct = dbContext.Products.FirstOrDefault(p => p.ProductId == product.ProductId);
+                if (updateProduct != null)
+                {
+                    updateProduct.ProductName = product.ProductName;
+                    updateProduct.CategoryId = product.CategoryId;
+                    updateProduct.Image = product.Image;
+                    updateProduct.UnitPrice = product.UnitPrice;
+                    updateProduct.UnitsInStock = product.UnitsInStock;
+                }
+                dbContext.SaveChanges();
+            }
+            return RedirectToAction("List");
+        }
+        [HttpPost]
+        public IActionResult DeleteProduct(int deleteId)
+        {
+            var product = dbContext.Products.FirstOrDefault(p => p.ProductId == deleteId);
+            dbContext.Products.Remove(product);
+            dbContext.SaveChanges();
+            return RedirectToAction("List");
+        }
+
+        public void Load(int? categoryId)
+        {
+            if (categoryId != 0 && categoryId != null)
+            {
+                Products = dbContext.Products.Include(p => p.Category).
+                    Where(p => p.CategoryId == categoryId).ToList();
+            }
+            else
+            {
+                Products = dbContext.Products.Include(p => p.Category).ToList();
+            }
+            Categories = dbContext.Categories.ToList();
+
+        }
+
+        public IActionResult List(int? pr, int categoryId = 0)
+        {
+            
+            Load(categoryId);
+            Console.WriteLine();
+            if (pr != null && pr != 0)
+            {
+                ViewBag.productId = pr;
+                CurrentProduct = dbContext.Products.Include(p => p.Category).Single(p => p.ProductId == pr);
+                ViewBag.CurrentProduct = CurrentProduct;
+
+            }
+            ViewBag.PickCategory = categoryId;
+            ViewBag.Categories = Categories;
+            return View(Products);
+        }
+
+        
+        
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PRN231/code/SE1702-PRN231/SE1702-PRN231/Program.cs
@@ -0,0 +1,41 @@
+using SE1702_PRN231.Models;
+
+namespace SE1702_PRN231
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<MySaleDBContext>();
+
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
